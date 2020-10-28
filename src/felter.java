8ba6ff81f200b06cb2c2593d76fe77f763929c6b
Rasmus Pedersen <rasmus.jdp@hotmail.com>
2020-10-28 17:30:41 +0100

    Tilføjet beskrivelser til felterne

--- src/felter.java
+++ src/felter.java
@@ -3,17 +3,17 @@ public class felter {
 
     static String[] beskeder = {
             "","",
-            "Du brød ind i et forladt tårn og fandt 250 guld!",
-            "Crater",
-            "Palace gates",
-            "Cold Desert",
-            "Walled city",
-            "Monastery",
-            "Black cave",
-            "Huts in the mountain",
-            "The Werewall",
-            "The pit",
-            "Goldmine"
+            "Tower - Du brød ind i et forladt tårn og fandt 250 guld!",
+            "Crater - Du bliver skræmt af noget nede i krateret og taber 100 guld!",
+            "Palace gates - Du stopper foran portene til et palads hvor der bliver givet 100 guld ud til forbipasserende!",
+            "Cold Desert - For at overleve natten køber du et tæppe til 20 guld!",
+            "Walled city - Du kommer ind bag bymurene, hvor du sælger råvarer du har fået samlet til 180 guld!",
+            "Monastery - Klostret påvirker ikke din guldbeholdning",
+            "Black cave - Du skal ned i en mørk gråtte, men mangler en fakkel. Den koster 70 guld!",
+            "Huts in the mountain - I bjergene kommer du forbi nogle hytter. Du hjælper landsbyboere med at bygge et tag, og de betaler din 60 guld!",
+            "The Werewall - Du betaler 80 guld for at se Varulvvæggen",
+            "The pit - Du hopper over et hul på din vej, men taber desværre 50 guld!",
+            "Goldmine - Du fandt en guldmine fyldt med guld. Nok til 650 guldmønter!"
     };
 
     //Returnerer værdien af det felt man har ramt
